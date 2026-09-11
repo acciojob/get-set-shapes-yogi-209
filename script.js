e09a9357350f4c1b22540ce7615a67e1fe0f239e
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-11 12:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
-
-class Square extends Animal {}
+class Rectangle {
+	constructor(width,height)
+	{
+		this.width=width;
+		this.height=height;
+	}
+	.get width()
+	{
+		return this.width;
+	}
+	.get height(){
+		return this.height; 
+	}
+	getArea()
+	{
+		return width*height;
+	}
+}
+class Square extends Animal{
+	constructor(side)
+	{
+		this.side=side;
+	}
+	getPerimeter()
+	{
+		return 4*side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
